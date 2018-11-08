658e0c1a4023c9a688f00670a4e257ec062f350c
codenuel <codenuel@gmail.com>
2018-11-08 10:22:56 +0100

    Updated the transfer flow

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,6 @@ var request = require('request');
 var raveCtrl = require('../controllers/controller');
 require('dotenv').config();
 
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
@@ -20,49 +18,30 @@ router.get('/', function(req, res, next) {
       banks = JSON.parse(body);
       // console.log(banks)
       res.render('index', { title: '', banks : banks.data });
-
   });  
-
-  
 });
 
 router.post('/account-verify', function(req, res, next){
-
-  // var bankcode = req.body.recipient_account;
-  // var recipientacct  = req.body.recipient_bank;
-
-  // console.log(bankcode)
-  // console.log(recipientacct)
   req.body.PBFPubKey = process.env.PUBLIC_KEY
-
   var options = { method: 'POST', 
-                // url: 'https://ravesandboxapi.flutterwave.com/flwv3-pug/getpaidx/api/resolve_account',
                 url: 'https://ravesandbox.azurewebsites.net/flwv3-pug/getpaidx/api/resolve_account',
                 form: req.body,
                 headers: { 'content-type': 'application/json' }
-                  
                 };
   request(options, function (error, response, body) {
       if (error) throw new Error(error);
-
       var respp = JSON.parse(body);
       console.log(respp);
-
       res.status(200).json(respp);
-
   });
-
-
 });
-
 
 router.get('/verify-tx', function(req, res, next){
 
   var reference = res.query.ref;
-  var SECRET_KEY  = process.env.PUBLIC_KEY;
-  
-  console.log(reference);
-  console.log(SECRET_KEY);
+  var SECRET_KEY  = process.env.SECRET_KEY;
+  console.log(reference)
+  console.log(SECRET_KEY)
 
   var options = { 
                   method: 'POST', 
@@ -76,13 +55,34 @@ router.get('/verify-tx', function(req, res, next){
 
       var respp = JSON.parse(body);
       console.log(respp);
-
       res.status(200).json(respp);
-
   });
-  
-
 });
 
+router.post('/transfer', function(req, res, next) {
+  var reference = req.query.flwref;
+  var seckey = process.env.SECRET_KEY;
+  var account_bank = req.query.bankcode;
+  var account_number = req.query.accountno;
+  var amount = req.query.amount;
+
+  console.log(reference)
+  var options = { 
+                  method: 'POST', 
+                  url: 'https://ravesandboxapi.flutterwave.com/v2/gpx/transfers/create',
+                  form: { account_bank:account_bank, account_number:account_number, amount:amount, seckey:seckey, reference:reference },
+                  headers: { 'content-type': 'application/json' }
+                };
+  request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+
+      var resp = JSON.parse(body);
+      console.log(resp);
+
+      // res.status(200).json(respp);
+      res.render('success', {title: 'Your transfer to ' + account_number + ' was successful. Thank you for banking with us!'});
+
+  }); 
+});
 
 module.exports = router;
